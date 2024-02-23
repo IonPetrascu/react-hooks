a278451c14ce8b62d105ee473bf81cdcc58862ae
Ion Petrascu <ionpetrascuc0318@gmal.com>
2024-02-23 14:51:14 +0200

    useReducer counter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useReducer } from 'react';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment': {
+      return {
+        count: state.count + 1,
+      };
+    }
+
+    case 'decrement': {
+      return {
+        count: state.count - 1,
+      };
+    }
+
+    case 'reset': {
+      return {
+        count: action.payload,
+      };
+    }
+
+    default:
+      {
+        return state;
+      }
+     
+  }
+};
 const Main = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+  });
+  const increment = () => {
+    dispatch({
+      type: 'increment',
+    });
+  };
+  const decrement = () => {
+    dispatch({
+      type: 'decrement',
+    });
+  };
+  const reset = () => {
+    dispatch({
+      type: 'reset',
+      payload: 0,
+    });
+  };
   return (
-    <div>Main</div>
-  )
-}
+    <div>
+      <p>{state.count}</p>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
