55ab745cf858fd0894d77b48103d8643e97963f3
Ion Petrascu <ionpetrascuc0318@gmal.com>
2024-02-23 13:51:55 +0200

    useEffect Interval created

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+const Interval = ({ speedInterval }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, speedInterval);
+
+    return () => clearInterval(interval);
+  }, [speedInterval]);
+
+  return <div>{count}</div>;
+};
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  const [speedInterval, setSpeedInterval] = useState();
 
-export default Main
+  const handleClickInterval = (speed) => {
+    setSpeedInterval(speed);
+  };
+
+  return (
+    <div>
+      <Interval speedInterval={speedInterval} />
+      <button onClick={() => handleClickInterval(100)}>100ms</button>
+      <button onClick={() => handleClickInterval(500)}>500ms</button>
+      <button onClick={() => handleClickInterval(1000)}>1s</button>
+    </div>
+  );
+};
+
+export default Main;
